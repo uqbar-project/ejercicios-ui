38860e05163b4c4383aca9b3697f06b6772b6675
nnydjesus <nnydjesus>
2012-07-13 20:05:35 +0000

    comentario para el classloader
    git-svn-id: https://xp-dev.com/svn/uqbar/examples/ui@203 e1a88654-4610-4beb-b2e6-8ed2f74c6698

--- arena/celulares/src/org/uqbar/edu/paiu/examples/celulares/ui/arena/CelularApplication.java
+++ arena/celulares/src/org/uqbar/edu/paiu/examples/celulares/ui/arena/CelularApplication.java
@@ -3,10 +3,17 @@ package org.uqbar.edu.paiu.examples.celulares.ui.arena;
 import org.uqbar.arena.Application;
 import org.uqbar.arena.windows.Window;
 
+/**
+ * Correr esta clase con el siguiente argument
+ * 
+ * -Djava.system.class.loader=org.uqbar.arena.aop.ArenaClassLoader
+ *
+ */
 public class CelularApplication extends Application {
 
 	public static void main(String[] args) {
 		new CelularApplication().start();
+		
 	}
 
 	@Override
