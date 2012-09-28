fe9d45a7e06b09d9f7e200ccd505c7dfe6dc17e2
fdodino <fdodino>
2012-09-28 22:16:03 +0000

    Iteracion 3: Las acciones sumar y restar son via ajax
    git-svn-id: https://xp-dev.com/svn/uqbar/examples/ui@556 e1a88654-4610-4beb-b2e6-8ed2f74c6698

--- web/wicket/contador-ui-wicket/trunk/src/main/java/uqbar/contador/ui/wicket/HomePage.java
+++ web/wicket/contador-ui-wicket/trunk/src/main/java/uqbar/contador/ui/wicket/HomePage.java
@@ -1,8 +1,9 @@
 package uqbar.contador.ui.wicket;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -11,21 +12,27 @@ public class HomePage extends WebPage {
 	private int contador;
 	
     public HomePage(final PageParameters parameters) {
-        // TODO Add your page's components here
-        add(new Label("contador", new PropertyModel(this, "contador")));
-        add(new Link<Object>("sumar") {
+        final Label labelContador = new Label("contador", new PropertyModel(this, "contador"));
+        add(labelContador);
+        labelContador.setOutputMarkupId(true);
+        add(new AjaxLink<Object>("sumar") {
 
 			@Override
-			public void onClick() {
-				contador++;
+			public void onClick(AjaxRequestTarget target) {
+				if (target != null) {
+					contador++;
+					target.addComponent(labelContador);					
+				}
 			}
 		});
-		add(new Link<Object>("restar") {
+		add(new AjaxLink<Object>("restar") {
 			@Override
-			public void onClick() {
-				contador--;
+			public void onClick(AjaxRequestTarget target) {
+				if (target != null) {
+					contador--;
+					target.addComponent(labelContador);					
+				}
 			}
-		});    	
+		});
     }
 }
-
