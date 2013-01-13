be6b1d363da20222347ace20ce41ce0a33408803
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2013-01-13 19:29:38 -0300

    Usando GroovyApplication

--- arena/videoclub-arena-groovy/src/main/groovy/uqbar/videoclub/arena/VideoclubApplication.groovy
+++ arena/videoclub-arena-groovy/src/main/groovy/uqbar/videoclub/arena/VideoclubApplication.groovy
@@ -1,12 +1,10 @@
 package uqbar.videoclub.arena
 
 import org.uqbar.arena.Application
-import org.uqbar.arena.groovy.dsl.GroovyArenaExtensions;
-import org.uqbar.arena.widgets.Panel;
-import org.uqbar.arena.windows.MainWindow;
+import org.uqbar.arena.groovy.GroovyApplication
 import org.uqbar.arena.windows.Window
 
-import tadp.blocbaster.daos.Videoclub;
+import tadp.blocbaster.daos.Videoclub
 
 /**
  * Punto de entrada para ejecutar la aplicacion del videoclub con el framework
@@ -14,22 +12,20 @@ import tadp.blocbaster.daos.Videoclub;
  * 
  * @author npasserini
  */
-class VideoclubApplication extends Application {
-  
-    static {
-      GroovyArenaExtensions
-      Class.metaClass {
-        getHome = {  Videoclub.instance.getHome(delegate) }
-      }
-    }
-  
-	static void main(String[] args) {
-	 new VideoclubApplication().start()
-	}
+class VideoclubApplication extends GroovyApplication {
 
-	@Override
-	Window<?> createMainWindow() {
-		new BuscarSociosWindow(this)
-	}
+  static {
+    Class.metaClass {
+      getHome = {  Videoclub.instance.getHome(delegate) }
+    }
+  }
   
+  static void main(String[] args) {
+    new VideoclubApplication().start()
+  }
+
+  @Override
+  Window<?> createMainWindow() {
+    new BuscarSociosWindow(this)
+  }
 }
