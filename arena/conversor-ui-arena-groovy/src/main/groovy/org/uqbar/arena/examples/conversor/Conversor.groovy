a0048758e51cb3b426195836598ee60fda7ea29c
flbulgarelli <flbulgarelli@yahoo.com.ar>
2013-02-17 21:20:54 +0000

    Eliminando getters y setters del conversor
    git-svn-id: https://xp-dev.com/svn/uqbar/examples/ui@751 e1a88654-4610-4beb-b2e6-8ed2f74c6698

--- arena/conversor-ui-arena-groovy/src/main/groovy/org/uqbar/arena/examples/conversor/Conversor.groovy
+++ arena/conversor-ui-arena-groovy/src/main/groovy/org/uqbar/arena/examples/conversor/Conversor.groovy
@@ -4,35 +4,10 @@ import org.uqbar.commons.utils.Observable
 
 @Observable
 class Conversor {
-  private double millas
-  private double kilometros
-
-  // ********************************************************
-  // ** Acciones
-  // ********************************************************
+  double millas
+  double kilometros
 
   void convertir() {
     kilometros = millas * 1.60934
   }
-
-  // ********************************************************
-  // ** Atributos
-  // ********************************************************
-
-  double getMillas() {
-    this.millas
-  }
-
-  void setMillas(double millas) {
-    this.millas = millas
-  }
-
-  double getKilometros() {
-    this.kilometros
-  }
-
-  void setKilometros(double kilometros) {
-    this.kilometros = kilometros
-  }
-
 }
