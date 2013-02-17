4fedf7748d6caec8381e78c31fcb46d46749b90c
flbulgarelli <flbulgarelli@yahoo.com.ar>
2013-02-17 21:27:12 +0000

    Agregando ejemplo con extensiones
    git-svn-id: https://xp-dev.com/svn/uqbar/examples/ui@769 e1a88654-4610-4beb-b2e6-8ed2f74c6698

---
+++ arena/videoclub-arena-groovy/src/main/groovy/uqbar/videoclub/arena/extensions/BuscarSociosWindow.groovy
@@ -0,0 +1,111 @@
+package uqbar.videoclub.arena.extensions
+
+import java.text.SimpleDateFormat
+import java.util.Arrays
+
+import org.uqbar.arena.actions.MessageSend;
+import org.uqbar.arena.layout.ColumnLayout
+import org.uqbar.arena.widgets.Control
+import org.uqbar.arena.widgets.Label
+import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.widgets.Selector
+import org.uqbar.arena.widgets.TextBox
+import org.uqbar.arena.widgets.tables.Column
+import org.uqbar.arena.widgets.tables.Table
+import org.uqbar.arena.windows.Dialog
+import org.uqbar.arena.windows.WindowOwner
+import org.uqbar.commons.model.SearchByExample
+import org.uqbar.lacar.ui.model.Action
+
+import tadp.blocbaster.daos.Videoclub
+import tadp.blocbaster.entidades.Socio
+
+import com.uqbar.commons.collections.Transformer
+
+/**
+ * Representa la ventana de busqueda de socios del videoclub.
+ * 
+ * @author npasserini
+ */
+class BuscarSociosWindow extends SearchWindow {
+
+  BuscarSociosWindow(WindowOwner owner) {
+    super(owner, new SearchByExample(Socio.home))
+  }
+
+  @Override
+  void createMainTemplate(Panel formBuilder) {
+    title = "Buscador de Socios"
+    taskDescription = "Ingrese los parámetros de búsqueda"
+    
+    super.createMainTemplate(formBuilder)
+  }
+
+  @Override
+  void createFormPanel(Panel mainPanel) {
+    mainPanel.describe {
+      panel(contents: "example") {
+        layout = new ColumnLayout(2)
+        
+        labeledTextBox("Nombre", "nombre")
+
+        labeledTextBox("Direccion", "direccion")
+
+        labeledSelector(
+          label: "Estado", 
+          value: "estado", 
+          options: Socio.Estado.values())
+      }
+    }
+  }
+
+  @Override
+  void describeResultsGrid(Table table) {
+    table.describe {
+      heigth = 300
+      width = 600
+      
+      column(contents: "nombre") {
+        title = "Nombre"
+        fixedSize = 200
+      }
+      column(contents: {it.fecha.format("dd/MM/yyyy")}) {
+        title = "Fecha de ingreso"
+        fixedSize = 200
+      }
+      column(contents: "direccion") {
+        title = "Direccion"
+        fixedSize = 200
+      }
+    }
+  }
+
+  @Override
+  void addActions(Panel actionsPanel) {
+    super.addActions(actionsPanel)
+    
+    actionsPanel.describe {
+      button {
+        caption = "Nuevo Socio"
+        onClick { this.crearSocio() }
+      }
+    }
+  }
+
+  // ********************************************************
+  // ** Acciones
+  // ********************************************************
+
+  void crearSocio() {
+    new CrearSocioDialog(this).describe {
+      onAccept { this.modelObject.search() }
+      open()
+    }
+  }
+
+  @Override
+  Dialog createEditor(selected) {
+    new ModificarSocioDialog(this, selected)
+  }
+
+}
